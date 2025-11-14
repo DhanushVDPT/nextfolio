b0cc7055648bf7ae1e5c8f46333039c34ec2deca
DhanushVDPT <be19b034@smail.iitm.ac.in>
2025-11-14 18:21:50 +0530

    about redo

--- pages/about.js
+++ pages/about.js
@@ -9,6 +9,9 @@ export default function Page() {
                 <title>Dhanush | About</title>
             </Head>
             <div class="aboutme2">
+                <div class="dpimg">
+                    <Image id="aboutimg" className="aboutmeimg" alt="dhanush" src="/dpimg.jpg" width={280} height={280}></Image>
+                </div>
                 <div class="abouttext">
                     <p id="hi">About Me</p>
                     <p>
@@ -16,9 +19,6 @@ export default function Page() {
                         <br></br>My interests (other than teaching) include cricket and movies. I am an avid binge-watcher of cinema, cricket, TV shows, documentaries, anime, etc. (basically anything and everything). I love analyzing cricket and cinema.<br></br>
                         <br></br>I joined this fellowship because I have always loved teaching. I grew up surrounded by many of my relatives who were/are teachers. In my first semester of college, I got the chance to teach a 10th grade classroom in a local government school twice a week as part of an NSS project. Since then I wanted to pursue teaching as a profession. Luckily I came across Teach For India during a Pre-Placement Talk session in my college.<br></br>
                     </p>
-                </div>
-                <div class="dpimg">
-                    <Image id="aboutimg" className="aboutmeimg" alt="dhanush" src="/dpimg.jpg" width={280} height={280}></Image>
                 </div>
             </div>
             <br></br><br></br>
